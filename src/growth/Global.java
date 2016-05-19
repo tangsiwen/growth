dfa7f9a0033cdcd722f58d7e4afade26ee572861
tangsw <707753010@qq.com>
2016-05-19 18:15:39 +0800

    UUID

--- src/growth/Global.java
+++ src/growth/Global.java
@@ -1,4 +1,7 @@
 package growth;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
 public class Global {
   /**
    * 禁止外部新建Global对象
@@ -9,5 +12,20 @@ public class Global {
 	   if(global==null)
 		   global = new Global();
 	   return global;
+   }
+   public  String UUID_32(){
+   	return RandomStringUtils.randomAlphanumeric(32);
+   }
+   public  String UUID_4(){
+   	return RandomStringUtils.randomAlphanumeric(4);
+   }
+   public  String UUID_5(){
+   	return RandomStringUtils.randomAlphanumeric(5);
+   }
+   public  String UUID_16(){
+   	return RandomStringUtils.randomAlphanumeric(16);
+   }
+   public  String UUID_64(){
+   	return RandomStringUtils.randomAlphanumeric(64);
    }
 }
